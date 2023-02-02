c85b9d3364436f82bc04ca46d2193a8dc050b569
dimisavva <119276394+dimisavva@users.noreply.github.com>
2023-02-02 09:18:03 -0800

    finish Nav, stub up Landing page

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,21 @@ import { useState } from 'react';
 
 // Styling
 import './App.css';
+import './fonts/NunitoSans-Regular.ttf'
+import './fonts/NunitoSans-SemiBold.ttf'
 
 // Components
-
+import Nav from './components/Nav';
 
 function App() {
   return (
     <>
+    <Nav />
+    <main>
+      <Routes>
+        <Route path='/' element={<Landing />}/>
+      </Routes>
+    </main>
     </>
   );
 }
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -5,7 +5,15 @@ import Logo from '../assets/ds_logo.png'
 const Nav = (props) => {
   return ( 
     <header className='nav-bar'>
-      <NavLink to='/' id='logo'><img src={Logo} alt="" className='logo'/></NavLink>
+    <NavLink to='/bio' className='link'>About</NavLink>
+      <p>|</p>
+      <NavLink to='/projecs' className='link'>Projects</NavLink>
+      <NavLink to='/' id='logo'>
+        <img src={Logo} alt="" className='logo'/>
+      </NavLink>
+      <NavLink to='/contact' className='link'>Contact</NavLink>
+      <p>|</p>
+      <NavLink to='/resume' className='link'>Resume</NavLink>
     </header>
    );
 }
